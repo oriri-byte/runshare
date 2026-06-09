b1b599cfb5cdbbf29db4a3583f454e8d1a69d06a
oriri-byte <179485773+oriri-byte@users.noreply.github.com>
2026-06-09 18:32:19 +0900

    feat: マイグレーションを実行

---
+++ prisma/migrations/20260609092812_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "distance" DOUBLE PRECISION NOT NULL,
+    "estimatedMinutes" INTEGER,
+    "routePoints" JSONB NOT NULL,
+    "startLat" DOUBLE PRECISION NOT NULL,
+    "startLng" DOUBLE PRECISION NOT NULL,
+    "endLat" DOUBLE PRECISION NOT NULL,
+    "endLng" DOUBLE PRECISION NOT NULL,
+    "area" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" TEXT NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Course" ADD CONSTRAINT "Course_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
